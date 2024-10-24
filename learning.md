40fb64b8c021888e3e1644f1d8865c81be8df8a9
alokkumarpdubey <alokkumarpdubey@gmail.com>
2024-10-24 21:14:12 +0530

    exploring GET, POST methods

--- learning.md
+++ learning.md
@@ -23,3 +23,14 @@
 - learn about caret vs tilde in package.json
 - updated script in package.json : "start": "node src/app.js", "dev": "nodemon src/app"
 - tested server : npm run dev
+- routing handlers
+    orders in which routing handlers are defined matter
+    first match first serve
+    /hello and /test are defined before /, so they will be matched first
+    / will be matched last
+    if no match is found, it will return "Cannot GET /"
+- using postman to test api
+    install postman
+    test api : http://localhost:3000/hello
+    test api : http://localhost:3000/test
+    test api : http://localhost:3000/
--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello from the server.");
-});
+// routing handlers
 
+// this will match all the HTTP methoda api calls
 app.use("/test", (req, res) => {
-    res.send("Test from the server.");
+  res.send("Test from the server.");
 });
 
+// this will match only GET requests to user
+app.get("/user", (req, res) => {
+  res.send({ name: "Alok", age: 30 });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Creating a new user");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
