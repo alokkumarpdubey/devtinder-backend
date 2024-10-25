a0871427441bf6514ce7887c2b06311c6165c763
alokkumarpdubey <alokkumarpdubey@gmail.com>
2024-10-25 07:18:20 +0530

    exploring multiple route handlers and next();

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require("express");
 const app = express();
 
 // routing handlers
-
-// "/user"
-// "/us?er"
-// "/us+er"
-// "/us*er"
-// "/u(se)?r"
-
-app.get("/user/:userId", (req, res) => {
+app.get("/user/:userId", (req, res, next) => {
   console.log("req.query", req.query);
   console.log("req.params", req.params);
   res.send({ name: "Alok", age: 30 });
+});
+
+app.get("/profile", (req, res, next) => {
+  next();
+  console.log("profile 1");
+}, (req, res, next) => {
+  next();
+  console.log("profile 2");
+}, (req, res, next) => {
+  next();
+  console.log("profile 3");
+}, (req, res, next) => {
+  console.log("profile 4");
+  res.send("profile 4");
 });
 
 app.listen(3000, () => {
